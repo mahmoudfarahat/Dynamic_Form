b4866e563a6a2f5004eb1ff8f97fc45b6e11e330
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-12-19 11:12:10 +0200

    1.1

--- DynamicForms.client/src/app/dynamic-form/dynamic-form.component.ts
+++ DynamicForms.client/src/app/dynamic-form/dynamic-form.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { QuestionBase } from '../Services/question-base';
 import { FormGroup } from '@angular/forms';
 import { QuestionControlService } from '../Services/question-control.service';
+import { TextboxQuestion } from '../Services/question-textbox';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -20,11 +21,27 @@ export class DynamicFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.qcs.toFormGroup(this.questions);
+    console.log(2)
 
   }
 
   onSubmit() {
+    console.log(this.form.value)
     this.payLoad = JSON.stringify(this.form.getRawValue());
   }
 
+  addInput(){
+   const input = new TextboxQuestion({
+      key: 'SecondName',
+      label: 'First name',
+      value: 'Bombasto',
+      required: true,
+      order: 1
+    })
+
+console.log(this.questions.push(input))
+this.form = this.qcs.toFormGroup(this.questions);
+console.log(this.questions)
+
+  }
 }
