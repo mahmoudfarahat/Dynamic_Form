7be6d3f4cd678c4a555c16c9e2ce91a3f61e0814
Mahmoud <mahmoud.ped@gmail.com>
2023-12-21 15:01:55 +0200

    1.2

--- DynamicForms.client/src/app/input-form/input-form.component.ts
+++ DynamicForms.client/src/app/input-form/input-form.component.ts
@@ -5,81 +5,74 @@ import { every } from 'rxjs';
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
-  styleUrls: ['./input-form.component.css']
+  styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent implements OnInit {
+  @Output() newItemEvent = new EventEmitter<any>();
+  needNumber = false;
+  needOptions = false
+  constructor() {}
 
-@Output()  newItemEvent = new EventEmitter<any>();
-needNumber= false
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    
+  inputForm = new FormGroup({
+    key: new FormControl(''),
+    type: new FormControl('', [Validators.required]),
+    label: new FormControl('', [Validators.required]),
+    labelArray: new FormArray([]),
+    optionArray:new FormArray([]),
+    order: new FormControl(''),
+    option: new FormControl(''),
+    number: new FormControl(''),
+    required: new FormControl(''),
+  });
+  get getOptionArray(){
+    return this.inputForm.get('optionArray') as FormArray;
+  }
+  get getLabelArray() {
+    return this.inputForm.get('labelArray') as FormArray;
   }
 
- inputForm = new FormGroup({
-  key:new FormControl(''),
-  type:new FormControl('',[Validators.required]),
-  label:new FormControl('',[Validators.required]),
-  labelArray: new FormArray([]),
-  order:new FormControl(''),
-  number:new FormControl(''),
-  required:new FormControl(''),
- })
-get getLabelArray()
-{
-  return this.inputForm.get('labelArray') as FormArray
-}
+  addLabel(event: any) {
+    const currentValue = event.target.value;
+    const previousValue = this.getLabelArray.length;
 
- addLabel(event:any){
-  const currentValue = event.target.value;
-  const previousValue = this.getLabelArray.length;
-
-  if (currentValue > previousValue) {
-    // Add form controls
-    for (let i = previousValue; i < currentValue; i++) {
-
-      this.getLabelArray.push(
-        new FormGroup({
-          text: new FormControl('')
-        })
-      );
+    if (currentValue > previousValue) {
+      // Add form controls
+      for (let i = previousValue; i < currentValue; i++) {
+        this.getLabelArray.push(
+          new FormGroup({
+            text: new FormControl(''),
+          })
+        );
+      }
+    } else if (currentValue < previousValue) {
+      // Remove form controls
+      for (let i = previousValue; i > currentValue; i--) {
+        this.getLabelArray.removeAt(i - 1);
+      }
     }
 
-  } else if (currentValue < previousValue) {
-    // Remove form controls
-    for (let i = previousValue; i > currentValue; i--) {
-      this.getLabelArray.removeAt(i - 1);
+    console.log(this.getLabelArray.value);
+  }
+  onSubmit() {
+    let label = this.inputForm.get('label')?.value;
+    let key = (label?.charAt(0).toLowerCase() + label!.slice(1))
+      .split(' ')
+      .join('');
+    this.inputForm.get('key')?.setValue(key);
+    this.newItemEvent.emit(this.inputForm.value);
+    console.log(this.inputForm.value);
+  }
+
+  onSelectChange(event: any) {
+    if (event.target.value == 'radio') {
+      this.needNumber = true;
+    }else if (event.target.value == 'dropdown')
+    {
+
+      this.needOptions = true;
     }
   }
 
-  console.log(this.getLabelArray.value);
- }
- onSubmit(){
-
-  let label = this.inputForm.get('label')?.value;
-  let key = (label?.charAt(0).toLowerCase() + label!.slice(1)).split(" ").join("")
- this.inputForm.get('key')?.setValue(key)
-  this.newItemEvent.emit(this.inputForm.value)
-  console.log(this.inputForm.value)
-
-
-
- }
-
- onSelectChange(event:any){
-  if(event.target.value =='radio'){
-
-this.needNumber = true
-
-  }
-
- }
-  // const input = new TextboxQuestion({
-  //   key: 'SecondName',
-  //   label: 'First name',
-  //   value: 'Bombasto',
-  //   required: true,
-  //   order: 1
-  // })
 }
